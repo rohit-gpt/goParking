6a13548da094fdc44836d7c33c084c9c9cd567f9
RohitGupta <rohitgpt@outlook.com>
2018-09-01 16:00:33 +0530

    Landing and pricing page created.

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res) {
 	res.render("landing");
 });
 
+app.get("/pricing", function(req, res) {
+	res.render("pricing");
+});
+
+app.get("/verifyPurchase", function(req, res) {
+	res.render("verify");
+});
+
 app.listen(8080, function() {
 	console.log("Server running on port 8080");
 });
--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250'
+      },
+    });
+  });
+
+$(document).ready(function(){
+    $('.timepicker').timepicker({
+    	minTime: '11:00:00',
+    	maxTime: '23:00:00'
+    });
+  });
+
+$(document).ready(function(){
+    $('select').formSelect();
+  });
