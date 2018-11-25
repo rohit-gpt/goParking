0ac2860016e9d1044a1cb456348ab0707d237f62
RohitGupta <rohitgpt@outlook.com>
2018-11-25 16:13:43 +0530

    Deploy for Finals

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -6,7 +6,18 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('input.autocomplete').autocomplete({
       data: {
-        "City Centre Shalimar Bagh": null
+        "City Centre Shalimar Bagh": null,
+        "Ambience Mall Gurgaon": null,
+        "MGF Metropolitan Gurgaon": null,
+        "MGF Metropolis Gurgaon": null,
+        "City Centre Mall Gurgaon": null,
+        "Sahara Mall Gurgaon": null,
+        "Mega Mall Gurgaon": null,
+        "Star Mall Gurgaon": null,
+        "Select City Walk Saket": null,
+        "City Centre Saket": null,
+        "DLF Promenade Vasant Kunj": null,
+        "Central Plaza Golf Course Road Gurgaon": null
       }
     });
   });
