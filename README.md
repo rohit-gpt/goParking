868b0209abb68af5d08ac37882a992bb407aee80
RohitGupta <rohitgpt@outlook.com>
2018-11-25 15:58:33 +0530

    Intermediate Commit

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # goParking
 Website that solves the purpose of parking issues in malls by providing online parking reservation services.
+
+
+// if(req.body.vehicle_category === "2w") {
+				// 	found.slots_2w.forEach(function(slot) {
+				// 		var check = false;
+				// 		for(var i=req.body.start_time-1; i<=(req.body.start_time-1+req.body.hours); i++) {
+				// 			if(slot[i] === true)
+				// 				check = true;
+				// 				break;
+				// 		}
+				// 		if(check === false) {
+				// 			Bookings.create({
+				// 				mall_name: req.body.mall_name,
+				// 				vehicle_type: req.body.vehicle_category,
+				// 				start: st,
+				// 				end_time: end,
+				// 				reg_no: req.body.reg_number,
+				// 				contact: req.body.contact,
+				// 				price: Price,
+				// 				grandtotal: gt
+				// 			}, function(err, newBooking) {
+				// 				if(err) {
+				// 					console.log(err);
+				// 					res.redirect("/");
+				// 				}
+				// 				else {
+				// 					for(var i=req.body.start_time-1; i<(req.body.start_time-1+req.body.hours); i++) {
+				// 						slot[i] = true
+				// 					}
+				// 					found.save();
+				// 					res.redirect("/booking/" + newBooking._id + "/verify");
+				// 				}
+				// 			});
+				// 		}
+				// 	});
+				// }
+<!--  -->
+				// else if(req.body.vehicle_category === "4w") {
+				// 	found.slots_4w.forEach(function(slot) {
+				// 		var check = false;
+				// 		for(var i=req.body.start_time-1; i<=(req.body.start_time-1+req.body.hours); i++) {
+				// 			if(slot[i] === true)
+				// 				check = true;
+				// 				break;
+				// 		}
+				// 		if(check === false) {
+				// 			Bookings.create({
+				// 				mall_name: req.body.mall_name,
+				// 				vehicle_type: req.body.vehicle_category,
+				// 				start: st,
+				// 				end_time: end,
+				// 				reg_no: req.body.reg_number,
+				// 				contact: req.body.contact,
+				// 				price: Price,
+				// 				grandtotal: gt
+				// 			}, function(err, newBooking) {
+				// 				if(err) {
+				// 					console.log(err);
+				// 					res.redirect("/");
+				// 				}
+				// 				else {
+				// 					for(var i=req.body.start_time-1; i<(req.body.start_time-1+req.body.hours); i++) {
+				// 						slot[i] = true;
+				// 					}
+				// 					found.save();
+				// 					res.redirect("/booking/" + newBooking._id + "/verify");
+				// 				}
+				// 			});
+				// 		}
+				// 	});
+				// }
--- app.js
+++ app.js
@@ -5,31 +5,32 @@ var bodyParser = require("body-parser");
 // var socket = require("socket.io");
 var alert = require("alert-node");
 
-var server = app.listen(process.env.PORT, process.env.ID, function() {
+var server = app.listen(process.env.PORT | 8000 , function() {
 	console.log("Server running");
 });
 
-// mongoose.connect("mongodb://localhost/goparkingdemo");
-mongoose.connect(process.env.DATABASEURL);
+mongoose.connect("mongodb://localhost/goparkingdemo");
+//mongoose.connect(process.env.DATABASEURL);
+
+// var bookingSchema = new mongoose.Schema({
+// 	mall_name: String,
+// 	vehicle_type: String,
+// 	from: Number,
+// 	till: Number,
+// 	reg_no: String,
+// 	contact: String,
+// 	price: Number,
+// 	grandtotal: Number
+// });
+
+// var Bookings = mongoose.model("Booking", bookingSchema);
 
 var mallSchema = new mongoose.Schema({
 	name: String,
 	parkings_2w: Number,
 	parkings_4w: Number,
-	occupied_2w: [
-		{
-			stt: Number,
-			edt: Number,
-			isOccupied: Boolean
-		}
-	],
-	occupied_4w: [
-		{
-			stt: Number,
-			edt: Number,
-			isOccupied: Boolean
-		}
-	]
+	slots_2w: [Array], 
+	slots_4w: [Array]
 });
 
 var Malls = mongoose.model("Mall", mallSchema);
@@ -69,12 +70,13 @@ app.post("/malls", function(req, res) {
 			console.log(err);
 		}
 		else {
-			for(var i=0;i<newMall.parking_2w;i++) {
-				newMall.occupied_2w.push(false);
+			for(var i=0;i<newMall.parkings_2w;i++) {
+				newMall.slots_2w.push([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]);
 			}
-			for(var j=0;j<newMall.parking_4w;j++) {
-				newMall.occupied_4w.push(false);
+			for(var i=0;i<newMall.parkings_4w;i++) {
+				newMall.slots_4w.push([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]);
 			}
+			newMall.save();
 			res.redirect("/admin");
 		}
 	});
@@ -85,7 +87,7 @@ app.get("/", function(req, res) {
 });
 
 app.post("/booking", function(req, res) {
-	Malls.find({name: req.body.mall_name}, function(err, found) {
+	Malls.findOne({name: req.body.mall_name}, function(err, found) {
 		if(err) {
 			res.redirect("/");
 			alert("Please Select Valid Mall Name");
@@ -96,8 +98,7 @@ app.post("/booking", function(req, res) {
 				res.redirect("/");
 				alert("Please Select Appropriate Time");
 			}
-			else
-			{
+			else {
 				var st = parseInt(clock[req.body.start_time-1][0] + clock[req.body.start_time-1][1]);
 				var end = st+parseInt(req.body.hours);
 				if(end===24) {
@@ -112,8 +113,7 @@ app.post("/booking", function(req, res) {
 					if(d.getDay() >= 1 && d.getDay() <= 4) {
 						Price = (parseInt(req.body.hours) * 15);
 					}
-					else
-					{
+					else {
 						Price = (parseInt(req.body.hours) * 25);
 					}
 				}
@@ -121,30 +121,61 @@ app.post("/booking", function(req, res) {
 					if(d.getDay() >= 1 && d.getDay() <= 4) {
 						Price = (parseInt(req.body.hours) * 25);
 					}
-					else
-					{
+					else {
 						Price = (parseInt(req.body.hours) * 40);
 					}
 				}
 				var gt = Price + 10;
-				Bookings.create({
-					mall_name: req.body.mall_name,
-					vehicle_type: req.body.vehicle_category,
-					start: st,
-					end_time: end,
-					reg_no: req.body.reg_number,
-					contact: req.body.contact,
-					price: Price,
-					grandtotal: gt
-				}, function(err, newBooking) {
-					if(err) {
-						console.log(err);
+
+				if(req.body.vehicle_category === "2w") {
+					for(var i=0; i < found.parkings_2w; i++) {
+						var isEmpty = true;
+						for(var j=parseInt(req.body.start_time)-1; j<=parseInt(req.body.start_time)-2+parseInt(req.body.hours); j++) {
+							if(found.slots_2w[i][j] === true) {
+								isEmpty=false;
+								break;
+							}
+						}
+
+						var updatedfound = found;
+
+						if(isEmpty === true) {
+							Bookings.create({
+								mall_name: req.body.mall_name,
+								vehicle_type: req.body.vehicle_category,
+								start: st,
+								end_time: end,
+								reg_no: req.body.reg_number,
+								contact: req.body.contact,
+								price: Price,
+								grandtotal: gt
+							}, function(err, newBooking) {
+								if(err) {
+									console.log(err);
+									res.redirect("/");
+								}
+								else {
+									for(var k=req.body.start_time-1; k<=parseInt(req.body.start_time) + parseInt(req.body.hours) - 2; k++) {
+										updatedfound.slots_2w[i][k] = true;
+									}
+									console.log(updatedfound);
+									Malls.findOneAndUpdate({name: req.body.mall_name}, updatedfound, function(err, updated) {
+										if(err) {
+											console.log(err);
+										}
+										else {
+											res.redirect("/booking/" + newBooking._id + "/verify");
+										}
+									})
+								}
+							});
+							break;
+						}
+					}
+					if(isEmpty === false) {
 						res.redirect("/");
 					}
-					else {
-						res.redirect("/booking/" + newBooking._id + "/verify");
-					}
-				});
+				}
 			}
 		}
 	});
@@ -162,6 +193,17 @@ app.get("/booking/:id/verify", function(req, res) {
 		else
 		{
 			res.render("verify", {booking:found});
+		}
+	});
+});
+
+app.get("/:id/payment", function(req, res) {
+	Bookings.findById(req.params.id, function(err, foundBooking) {
+		if(err) {
+			console.log(err);
+		}
+		else {
+			res.render("payment", {booking:foundBooking});
 		}
 	});
 });
